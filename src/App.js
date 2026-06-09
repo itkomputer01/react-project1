67916c5c43b19a5ae5e216ff313024b50eaa2c94
itkomputer01 <itkomputer01@gmail.com>
2026-06-09 21:38:25 +0700

    pesan perubahantesss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit XXX <code>src/App.js</code> and save to reload. OKAY
+          Edit XXX  cek ooooo ssdds <code>src/App.js</code> and save to reload. OKAY
         </p>
         <a
           className="App-link"
